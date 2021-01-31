d851f26fa5ba8e9594498d4e182e4742ab5fd754
Oksana <cpl.aksena@gmail.com>
2021-01-31 17:19:55 +0300

    first block rebuild

--- Girlfriendtelebot.py
+++ Girlfriendtelebot.py
@@ -1,66 +1,56 @@
 # Подключаем модуль случайных чисел
 import random
+
 # Подключаем модуль для Телеграма
 import telebot
+
 # Указываем токен
 bot = telebot.TeleBot('1487122552:AAGF8UugtdUTZbylEt8Mjo_n76JHdSJn1gY')
+
 # Импортируем типы из модуля, чтобы создавать кнопки
 from telebot import types
+
 # Заготовки для трёх предложений
-first = ["Сегодня — идеальный день для новых начинаний.","Оптимальный день для того, чтобы решиться на смелый поступок!","Будьте осторожны, сегодня звёзды могут повлиять на ваше финансовое состояние.","Лучшее время для того, чтобы начать новые отношения или разобраться со старыми.","Плодотворный день для того, чтобы разобраться с накопившимися делами."]
-second = ["Но помните, что даже в этом случае нужно не забывать про","Если поедете за город, заранее подумайте про","Те, кто сегодня нацелен выполнить множество дел, должны помнить про","Если у вас упадок сил, обратите внимание на","Помните, что мысли материальны, а значит вам в течение дня нужно постоянно думать про"]
-second_add = ["отношения с друзьями и близкими.","работу и деловые вопросы, которые могут так некстати помешать планам.","себя и своё здоровье, иначе к вечеру возможен полный раздрай.","бытовые вопросы — особенно те, которые вы не доделали вчера.","отдых, чтобы не превратить себя в загнанную лошадь в конце месяца."]
-third = ["Злые языки могут говорить вам обратное, но сегодня их слушать не нужно.","Знайте, что успех благоволит только настойчивым, поэтому посвятите этот день воспитанию духа.","Даже если вы не сможете уменьшить влияние ретроградного Меркурия, то хотя бы доведите дела до конца.","Не нужно бояться одиноких встреч — сегодня то самое время, когда они значат многое.","Если встретите незнакомца на пути — проявите участие, и тогда эта встреча посулит вам приятные хлопоты."]
+first = ["Я НЕ ВИДЕЛАСЬ С РОДСТВЕННИКАМИ И СТАРЫМИ ДРУЗЬЯМИ ЦЕЛУЮ ВЕЧНОСТЬ! ВЕЧНОСТЬ ИЗ-ЗА ТЕБЯ! КАК ТЫ МОЖЕШЬ УПРЕКАТЬ МЕНЯ В ЭТОМ?!"]
+second = ["Стало лучше, когда ты написал ^_^"]
+
+
 # Метод, который получает сообщения и обрабатывает их
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     # Если написали «Привет»
     if message.text == "Привет":
         # Пишем приветствие
-        bot.send_message(message.from_user.id, "Привет, сейчас я расскажу тебе гороскоп на сегодня.")
-        # Готовим кнопки
+        bot.send_message(message.from_user.id, "Так то лучше, пушистик! Я чёт так накидалась на НГ")
         keyboard = types.InlineKeyboardMarkup()
-        # По очереди готовим текст и обработчик для каждого знака зодиака
-        key_oven = types.InlineKeyboardButton(text='Овен', callback_data='zodiac')
-        # И добавляем кнопку на экран
+    # По очереди готовим текст и обработчик для каждого знака зодиака
+        key_oven = types.InlineKeyboardButton(text='Охренела?!', callback_data='Охренела')
+    # И добавляем кнопку на экран
         keyboard.add(key_oven)
-        key_telec = types.InlineKeyboardButton(text='Телец', callback_data='zodiac')
+        key_telec = types.InlineKeyboardButton(text='Как себя чувствуешь, зай?', callback_data='Как себя чувствуешь, зай')
         keyboard.add(key_telec)
-        key_bliznecy = types.InlineKeyboardButton(text='Близнецы', callback_data='zodiac')
-        keyboard.add(key_bliznecy)
-        key_rak = types.InlineKeyboardButton(text='Рак', callback_data='zodiac')
-        keyboard.add(key_rak)
-        key_lev = types.InlineKeyboardButton(text='Лев', callback_data='zodiac')
-        keyboard.add(key_lev)
-        key_deva = types.InlineKeyboardButton(text='Дева', callback_data='zodiac')
-        keyboard.add(key_deva)
-        key_vesy = types.InlineKeyboardButton(text='Весы', callback_data='zodiac')
-        keyboard.add(key_vesy)
-        key_scorpion = types.InlineKeyboardButton(text='Скорпион', callback_data='zodiac')
-        keyboard.add(key_scorpion)
-        key_strelec = types.InlineKeyboardButton(text='Стрелец', callback_data='zodiac')
-        keyboard.add(key_strelec)
-        key_kozerog = types.InlineKeyboardButton(text='Козерог', callback_data='zodiac')
-        keyboard.add(key_kozerog)
-        key_vodoley = types.InlineKeyboardButton(text='Водолей', callback_data='zodiac')
-        keyboard.add(key_vodoley)
-        key_ryby = types.InlineKeyboardButton(text='Рыбы', callback_data='zodiac')
-        keyboard.add(key_ryby)
-        # Показываем все кнопки сразу и пишем сообщение о выборе
-        bot.send_message(message.from_user.id, text='Выбери свой знак зодиака', reply_markup=keyboard)
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши Привет")
+        bot.send_message(message.from_user.id, text='Совершенно ничего не помню =( ', reply_markup=keyboard)
     else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+        bot.send_message(message.from_user.id, "Напиши нормально! Ты не можешь просто написать Привет?")
+
 # Обработчик нажатий на кнопки
 @bot.callback_query_handler(func=lambda call: True)
+
 def callback_worker(call):
-    # Если нажали на одну из 12 кнопок — выводим гороскоп
-    if call.data == "zodiac":
+    # Если нажали на одну из кнопок — выводим гороскоп
+    if call.data == 'Охренела':
         # Формируем гороскоп
-        msg = random.choice(first) + ' ' + random.choice(second) + ' ' + random.choice(second_add) + ' ' + random.choice(third)
+        msg = first
         # Отправляем текст в Телеграм
         bot.send_message(call.message.chat.id, msg)
+    if call.data == 'Как себя чувствуешь, зай':
+        # Формируем гороскоп
+        msg = second
+        # Отправляем текст в Телеграм
+        bot.send_message(call.message.chat.id, msg)
+
+
+
+
 # Запускаем постоянный опрос бота в Телеграме
 bot.polling(none_stop=True, interval=0)
-bot.polling(none_stop=True)
